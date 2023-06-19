181deb581f1ef212a6bcf84c54166e068c58959a
shivani <shivanigaonkar100@gmail.com>
2023-06-19 15:02:53 +0530

    math.py file

---
+++ math.py
@@ -0,0 +1,12 @@
+#Add implementation
+def add(x,y):
+    pass
+#Subtract implementation
+def subtracr(x,y):
+    pass
+ #Multiply implementation   
+ def multiply(x,y):
+    pass
+    
+ def divide(x,y):
+    pass
